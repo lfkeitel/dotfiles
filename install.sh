985980a64d705178cc3974ae0756bfe73ccf0382
Lee Keitel <lee@keitel.xyz>
2017-07-01 15:15:07 -0500

    Install base applications, install oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Install applications
+sudo apt install vim emacs curl zsh
+
 # Emacs
 ln -sfn "$DIR/emacs/.emacs.d/config" "$HOME/.emacs.d/config"
 ln -sfn "$DIR/emacs/.emacs.d/lisp" "$HOME/.emacs.d/lisp"
@@ -13,5 +16,5 @@ ln -sfn "$DIR/git/.git*" "$HOME"
 ln -sfn "$DIR/zsh/.zshrc" "$HOME"
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo "Oh My ZSH isn't installed. Make sure to do that."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
