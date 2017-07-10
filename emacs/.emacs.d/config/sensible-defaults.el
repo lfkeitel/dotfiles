fb423cafcb5b2e5b75324affd53a657a10af9859
Lee Keitel <lee@keitel.xyz>
2017-07-09 20:03:02 -0500

    Extracted window moves and sensible defaults

---
+++ emacs/.emacs.d/config/sensible-defaults.el
@@ -0,0 +1,5 @@
+; https://github.com/hrs/sensible-defaults.el
+(require 'sensible-defaults)
+(sensible-defaults/use-all-settings)
+(sensible-defaults/use-all-keybindings)
+(sensible-defaults/backup-to-temp-directory)
---
+++ emacs/.emacs.d/config/windows.el
@@ -0,0 +1,5 @@
+(require 'buffer-move)
+(global-set-key (kbd "<C-S-up>")     'buf-move-up)
+(global-set-key (kbd "<C-S-down>")   'buf-move-down)
+(global-set-key (kbd "<C-S-left>")   'buf-move-left)
+(global-set-key (kbd "<C-S-right>")  'buf-move-right)
