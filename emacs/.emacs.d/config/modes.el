58c15bccbdfb8fd2df39527e0c4b91a38a33bd48
Lee Keitel <lee@keitel.xyz>
2017-07-09 20:03:31 -0500

    Use smex for M-x auto complete

--- emacs/.emacs.d/config/modes.el
+++ emacs/.emacs.d/config/modes.el
@@ -5,3 +5,11 @@
 (use-package php-mode
   :ensure t
   :mode "\\.php\\'")
+
+(use-package smex
+  :ensure t
+  :config
+  (smex-initialize))
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
