13add5f13e1edc42b13af9c1a1f89703ec663284
Lee Keitel <lee@keitel.xyz>
2017-07-17 18:50:39 -0500

    Smoother scrolling in Emacs
    I still use the mouse a lot, sue me.

--- emacs/.emacs.d/config/windows.el
+++ emacs/.emacs.d/config/windows.el
@@ -3,3 +3,9 @@
 (global-set-key (kbd "<C-S-down>")   'buf-move-down)
 (global-set-key (kbd "<C-S-left>")   'buf-move-left)
 (global-set-key (kbd "<C-S-right>")  'buf-move-right)
+
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(4 ((shift) . 4))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
