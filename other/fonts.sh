56cad3bfdf8f7bc938cffd3720dc051191b0c192
Lee Keitel <lee@keitel.xyz>
2018-01-16 21:11:58 -0600

    Install Powerline fonts for iTerm & macOS
    The vim theme doesn't work on macOS with the
    normal Inconsolata font. A patched version
    for Powerline has to be used. These are only
    used for iTerm on mac. They won't be used
    anywhere else.

--- other/fonts.sh
+++ other/fonts.sh
@@ -8,22 +8,41 @@ library="/usr/local/share/fonts"
 if [[ $system_type = "Darwin" ]]; then
     library="$HOME/Library/Fonts"
 fi
+
+# Destination paths
 regular_font_out="$library/Inconsolata-Regular.ttf"
 bold_font_out="$library/Inconsolata-Bold.ttf"
+regular_font_powerline_out="$library/Inconsolata-Powerline-Regular.ttf"
+bold_font_powerline_out="$library/Inconsolata-Powerline-Bold.ttf"
 
+# Source paths
 regular_font="https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata-Regular.ttf"
 bold_font="https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata-Bold.ttf"
+regular_font_powerline="https://github.com/powerline/fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf"
+bold_font_powerline="https://github.com/powerline/fonts/raw/master/Inconsolata/Inconsolata%20Bold%20for%20Powerline.ttf"
 
+# If font doesn't exist, download to destination
 if [ ! -f $regular_font_out ]; then
-    sudo wget -O $regular_font_out $regular_font
+    sudo wget -q --show-progress -O $regular_font_out $regular_font
     if [[ $system_type != "Darwin" ]]; then RELOAD_FONT=1; fi
 fi
 
 if [ ! -f $bold_font_out ]; then
-    sudo wget -O $bold_font_out $bold_font
+    sudo wget -q --show-progress -O $bold_font_out $bold_font
     if [[ $system_type != "Darwin" ]]; then RELOAD_FONT=1; fi
 fi
 
+if [ ! -f $regular_font_powerline_out ]; then
+    sudo wget -q --show-progress -O $regular_font_powerline_out $regular_font_powerline
+    if [[ $system_type != "Darwin" ]]; then RELOAD_FONT=1; fi
+fi
+
+if [ ! -f $bold_font_powerline_out ]; then
+    sudo wget -q --show-progress -O $bold_font_powerline_out $bold_font_powerline
+    if [[ $system_type != "Darwin" ]]; then RELOAD_FONT=1; fi
+fi
+
+# Linux, reload font cache
 if [ $RELOAD_FONT -eq 1 ]; then
     fc-cache -f
 fi
