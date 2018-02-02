8c886af4fbfbcd48a4fc43967c4bb122dd929b6a
Lee Keitel <lee@keitel.xyz>
2018-02-01 18:58:52 -0600

    tmux - Removed old config
    All my machines use the new Tmux now so
    I don't need to maintain the old version.

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 [[ $DOTFILE_INSTALLER != 1 ]] && exit 0
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmux_config="$DIR/.tmux-new.conf"
 
 install_header "Setting up Tmux"
 
-# Ubuntu 16.04 is still old
-if [ "$(tmux -V)" = "tmux 2.1" ]; then
-    tmux_config="$DIR/.tmux-old.conf"
-fi
-
-if [ -f "$tmux_config" ]; then
-    ln -sfn "$tmux_config" "$HOME/.tmux.conf"
-fi
+ln -sfn "$DIR/.tmux.conf" "$HOME/.tmux.conf"
