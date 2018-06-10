8032c55ca2c509865e15457cdd32dda61d617559
Lee Keitel <lee@keitel.xyz>
2018-06-09 21:51:40 -0500

    No lazy nvm, yarn aliases, cat alias

--- node/nvm-hook.zsh
+++ node/nvm-hook.zsh
@@ -1,11 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
-# Lazy load nvm only when called
-nvm() {
-    unset -f nvm
-    if [ "$(uname)" = "Darwin" ]; then
-        [ -s "/usr/local/opt/nvm/nvm.sh"  ] && \. "/usr/local/opt/nvm/nvm.sh"
-    else
-        [ -s "$NVM_DIR/nvm.sh"  ] && \. "$NVM_DIR/nvm.sh"
-    fi
-    nvm "$@"
-}
+
+if [ "$(uname)" = "Darwin" ]; then
+    [ -s "/usr/local/opt/nvm/nvm.sh"  ] && \. "/usr/local/opt/nvm/nvm.sh"
+else
+    [ -s "$NVM_DIR/nvm.sh"  ] && \. "$NVM_DIR/nvm.sh"
+fi
