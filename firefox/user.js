0187f572b59727867242311d1238bbf88af20fc5
Lee Keitel <lee@keitel.xyz>
2019-02-05 20:43:04 -0600

    Disable media autoplay in Firefox

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,4 @@
 user_pref("widget.content.gtk-theme-override", "Vertex-Maia");
 user_pref("browser.urlbar.clickSelectsAll", true);
 user_pref("media.av1.enabled", true);
+user_pref("media.autoplay.default", 1);
