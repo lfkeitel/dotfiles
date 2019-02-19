07004d13ce08c7e7c133fafc5cb2fe0d6a763bb5
Lee Keitel <lee@keitel.xyz>
2019-02-19 14:24:12 -0600

    Add key remap autostart entry

--- configs/dconf/install.ps1
+++ configs/dconf/install.ps1
@@ -3,3 +3,4 @@ Import-Module (Join-Path $PSScriptRoot '..' '..' Utils)
 
 Write-Header 'Restoring Dconf Settings'
 Get-Content -Path (Join-Path $PSScriptRoot dconf-export.txt) -Encoding utf8 | dconf load /
+Add-FileLink -NoLink $PSScriptRoot/key_swap.desktop $HOME/.config/autostart/key_swap.desktop
