d020b29176aa21fce8b7904a93489662853e9cee
Lee Keitel <lee@keitel.xyz>
2019-04-12 16:46:08 -0500

    Fix env vars not being globally scoped

--- fish/nnn-setup.fish
+++ fish/nnn-setup.fish
@@ -1,7 +1,7 @@
-set -x NNN_USE_EDITOR 1
-set -x NNN_COPIER "$SCRIPTS_DIR/nnn_copier.sh"
-set -x NNN_BMS 'd:~/Documents;c:~/code;D:~/Downloads/;k:~/Desktop'
-set -x NNN_TMPFILE "/tmp/nnn"
+set -xg NNN_USE_EDITOR 1
+set -xg NNN_COPIER "$SCRIPTS_DIR/nnn_copier.sh"
+set -xg NNN_BMS 'd:~/Documents;c:~/code;D:~/Downloads/;k:~/Desktop'
+set -xg NNN_TMPFILE "/tmp/nnn"
 
 function n
     nnn $argv
