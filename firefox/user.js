5455227647f18394509572fe7e3ac028a6b7244d
Lee Keitel <lee@keitel.xyz>
2019-06-06 10:52:09 -0500

    Firefox disable middlemouse paste, vscode fold quickly

--- firefox/user.js
+++ firefox/user.js
@@ -3,3 +3,4 @@ user_pref("browser.urlbar.clickSelectsAll", true);
 user_pref("media.av1.enabled", true);
 user_pref("media.autoplay.default", 0);
 user_pref("security.webauth.u2f", true);
+user_pref("middlemouse.paste", false);
