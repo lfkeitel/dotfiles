ed92d64aacafb89250886f7c4d7b4bf478e9db56
Lee Keitel <lee@keitel.xyz>
2019-10-29 10:32:09 -0500

    Enable multiple modules with args
    ./install.py # Run all
    ./install.py vscode # Run one module
    ./install.py -- vscode firefox # Multiple modules with no args
    ./install.py vscode vscodium -- firefox -- golang force # Mutiple
    modules with args

--- install.py
+++ install.py
@@ -33,15 +33,53 @@ def main(args):
 
     print_banner("LEE'S DOTFILES", Color.BLUE)
 
-    if len(args) == 0:
+    if not args:  # Install all autorun modules
         for name, i in settings["installers"].items():
             if "autorun" in i and i["autorun"]:
                 run_installer(name)
         return 0
-    else:
-        name = args[0]
-        args = args[1:]
-        return run_installer(name, args)
+    elif len(args) == 1:  # Simple, run one module
+        return run_installer(args[0], [])
+    else:  # Run multiple modules, possibly with arguments
+        module_playbook = parse_args(args)
+
+        for name, play in module_playbook.items():
+            exit_code = run_installer(name, play["args"])
+            if exit_code != 0:
+                return exit_code
+
+
+def parse_args(args=[]):
+    modules = {}
+
+    if not args:
+        return modules
+
+    if args[0] == "--":
+        for arg in args[1:]:
+            modules[arg] = {"args": []}
+        return modules
+
+    while True:
+        try:
+            module_name = args.pop(0)
+        except:
+            break
+
+        module_args = []
+
+        while True:
+            try:
+                arg = args.pop(0)
+            except:
+                break
+            if arg == "--":
+                break
+            module_args.append(arg)
+
+        modules[module_name] = {"args": module_args}
+
+    return modules
 
 
 if __name__ == "__main__":
