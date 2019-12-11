4de3261e864fc5df34bdd63af5398e864f5681ab
Lee Keitel <lee@keitel.xyz>
2019-12-11 10:23:08 -0600

    fish: export bspwm desktop names

--- fish/vars.fish
+++ fish/vars.fish
@@ -4,19 +4,19 @@ set -xg TERMINAL st
 set -xg LEDGER_FILE "$HOME/finances/ledger.dat"
 
 # bspwm window names
-set -g BSPWM_WINDOW_1 "Web"
-set -g BSPWM_WINDOW_2 "Terminal"
-set -g BSPWM_WINDOW_3 "Email"
-set -g BSPWM_WINDOW_4 "Editor"
-set -g BSPWM_WINDOW_5 "5"
-set -g BSPWM_WINDOW_6 "6"
-set -g BSPWM_WINDOW_7 "7"
-set -g BSPWM_WINDOW_8 "8"
-set -g BSPWM_WINDOW_9 "9"
-set -g BSPWM_WINDOW_10 "10"
+set -xg BSPWM_WINDOW_1 "Web"
+set -xg BSPWM_WINDOW_2 "Terminal"
+set -xg BSPWM_WINDOW_3 "Email"
+set -xg BSPWM_WINDOW_4 "Editor"
+set -xg BSPWM_WINDOW_5 "5"
+set -xg BSPWM_WINDOW_6 "6"
+set -xg BSPWM_WINDOW_7 "7"
+set -xg BSPWM_WINDOW_8 "8"
+set -xg BSPWM_WINDOW_9 "9"
+set -xg BSPWM_WINDOW_10 "10"
 
 # name aliases for most used windows
-set -g BSPWM_WINDOW_WEB $BSPWM_WINDOW_1
-set -g BSPWM_WINDOW_TERMINAL $BSPWM_WINDOW_2
-set -g BSPWM_WINDOW_EMAIL $BSPWM_WINDOW_3
-set -g BSPWM_WINDOW_EDITOR $BSPWM_WINDOW_4
+set -xg BSPWM_WINDOW_WEB $BSPWM_WINDOW_1
+set -xg BSPWM_WINDOW_TERMINAL $BSPWM_WINDOW_2
+set -xg BSPWM_WINDOW_EMAIL $BSPWM_WINDOW_3
+set -xg BSPWM_WINDOW_EDITOR $BSPWM_WINDOW_4
