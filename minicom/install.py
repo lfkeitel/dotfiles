29a9b2f6e61a2e1eec86ac8ae3143f564f14e872
Lee Keitel <lee@keitel.xyz>
2020-01-17 16:22:36 -0600

    Add fast serial config for minicom

--- minicom/install.py
+++ minicom/install.py
@@ -10,10 +10,11 @@ from utils.utils import link_file
 class Main(Installer):
     def run(self):
         print_header("Setting up Minicom")
-        link_file(
-            Path(__file__).parent.joinpath(".minirc.dfl"),
-            Path.home().joinpath(".minirc.dfl"),
-        )
+        for file in ["dfl", "fast"]:
+            link_file(
+                Path(__file__).parent.joinpath(f".minirc.{file}"),
+                Path.home().joinpath(f".minirc.{file}"),
+            )
         install_pkg("minicom")
 
         if platform.is_linux:
