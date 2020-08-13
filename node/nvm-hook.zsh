250a0658106e29a8624e560819b8b81c1738d038
Lee Keitel <lee@keitel.xyz>
2020-08-13 15:39:53 -0500

    Remove remaining powershell scripts

--- node/nvm-hook.zsh
+++
@@ -1,7 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-
-if [ "$(uname)" = "Darwin" ]; then
-    [ -s "/usr/local/opt/nvm/nvm.sh"  ] && \. "/usr/local/opt/nvm/nvm.sh"
-else
-    [ -s "$NVM_DIR/nvm.sh"  ] && \. "$NVM_DIR/nvm.sh"
-fi
