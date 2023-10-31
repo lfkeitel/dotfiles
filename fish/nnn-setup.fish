b0d2b35a5cb949ac35e7ca8229b70a5dc90d344f
Lee Keitel <lee@keitel.xyz>
2023-10-31 08:44:06 -0500

    More config changes

--- fish/nnn-setup.fish
+++ fish/nnn-setup.fish
@@ -1,7 +1,21 @@
 set -xg NNN_USE_EDITOR 1
 set -xg NNN_COPIER "$SCRIPTS_DIR/nnn_copier.sh"
-set -xg NNN_BMS 'd:~/Documents;c:~/code;D:~/Downloads/;k:~/Desktop'
+set -xg NNN_BMS 'h:~;d:~/Documents;c:~/code;D:~/Downloads/;k:~/Desktop'
 set -xg NNN_TMPFILE "/tmp/nnn"
+set BLK "0B"
+set CHR "0B"
+set DIR "04"
+set EXE "06"
+set REG "00"
+set HARDLINK "06"
+set SYMLINK "06"
+set MISSING "00"
+set ORPHAN "09"
+set FIFO "06"
+set SOCK "0B"
+set OTHER "06"
+set -xg NNN_FCOLORS 'c1e2272e006033f7c6d6abc4'
+set -xg NNN_COLORS '#0a1b2c3d;1234'
 
 function n
     nnn $argv
