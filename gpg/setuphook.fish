8f2a670332f657bd45b55bad6f97a0fc06f62019
Lee Keitel <lee@keitel.xyz>
2023-12-21 19:25:39 -0600

    Make sure gpg-agent exists before starting

--- gpg/setuphook.fish
+++ gpg/setuphook.fish
@@ -1,6 +1,8 @@
 # Start gpg-agent if it's not running
-if [ -z (pidof gpg-agent 2> /dev/null) ]
-    gpg-agent --homedir $HOME/.gnupg --daemon --sh --enable-ssh-support > $HOME/.gnupg/env
-end
+if which gpg-agent
+    if [ -z (pidof gpg-agent 2> /dev/null) ]
+        gpg-agent --homedir $HOME/.gnupg --daemon --sh --enable-ssh-support > $HOME/.gnupg/env
+    end
 
-set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+end
