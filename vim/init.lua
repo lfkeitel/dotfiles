11c7fac212882ce82b37c594e846759759571007
Lee Keitel <lee@keitel.xyz>
2023-12-22 10:01:24 -0600

    Vim autoformat on save

--- vim/init.lua
+++ vim/init.lua
@@ -95,6 +95,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 local nvim_lsp = require'lspconfig'
 
