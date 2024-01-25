6d56018ef7bac585cccaba07c684bf612d976f14
Lee Keitel <lee@keitel.xyz>
2024-01-24 19:10:34 -0600

    Add opam installer

---
+++ opam/install.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from utils.installer import Installer
+from utils.chalk import print_header
+from utils.system import run_command, install_pkg
+from utils.shell import Hook, add_fish_hook
+from utils.utils import file_exists
+
+SCRIPT_DIR = Path(__file__).parent
+HOME_DIR = Path.home()
+
+class Main(Installer):
+    def run(self):
+        print_header("Setting up opam")
+        install_pkg("opam")
+
+        if not file_exists(HOME_DIR.joinpath(".opam", "config")):
+            run_command("opam init -n")
+
+        add_fish_hook(Hook.POST, "10-opam", SCRIPT_DIR.joinpath("setuphook.fish"))
+        run_command("opam install -y dune utop ocaml-lsp-server merlin")
---
+++ opam/setuphook.fish
@@ -0,0 +1,2 @@
+# opam configuration
+source /Users/lfkeitel/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
